a3b4a1cc12b48fcd919ba37b602e8c002153f4b5
Ahmed Mourad <89593254+mourad2213@users.noreply.github.com>
2026-05-28 20:21:08 +0300

    Remove unnecessary whitespace and add placeholder text

--- assets/anchored-popover.js
+++ assets/anchored-popover.js
@@ -1,6 +1,6 @@
 import { Component } from '@theme/component';
 import { debounce, requestIdleCallback } from '@theme/utilities';
-
+bbb
 /**
  * A custom element that manages the popover + popover trigger relationship for anchoring.
  * Calculates the trigger position and inlines custom properties on the popover element
